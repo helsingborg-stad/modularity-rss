07af027bd3cc22673cff92e82e9af796c1e3d040
Sebastian Thulin <sebastian.thulin@gmail.com>
2018-01-05 10:38:37 +0100

    View updates

--- source/php/views/feed.blade.php
+++ source/php/views/feed.blade.php
@@ -1,22 +1,24 @@
 @if(is_array($feed) && !empty($feed))
-    <ul id="{{ $sectionID }}" class="social social-feed-v2">
-        @foreach($feed as $item)
-            <li>
-                <a href="{{ $item['link'] }}" class="box box-news box-news-horizontal">
-                    <!-- <div class="box-image-container">
-                        <img src="https://unsplash.it/200/130/?image=600">
-                    </div> -->
-                    <div class="box-content">
-                        <h3 class="box-title text-highlight">{{ $item['title'] }}</h3>
-                        <time datetime="{{ $item['time_markup'] }}">{{ $item['time_readable'] }} {{ $translations['ago'] }}</time>
-                        <p>{{ $item['excerpt'] }}</p>
-                        <p><span class="link-item">{{ $translations['readmore'] }}</span></p>
-                    </div>
-                </a>
-            </li>
-        @endforeach
-    </ul>
+    <div class="{{$classes}}">
+        @if (!$hideTitle && !empty($post_title))
+            <h4 class="box-title">{!! apply_filters('the_title', $post_title) !!}</h4>
+        @endif
 
+        <ul id="{{ $sectionID }}" class="rss-feed rss-feed-v2">
+            @foreach($feed as $item)
+                <li>
+                    <a href="{{ $item['link'] }}" class="box box-news box-news-horizontal">
+                        <div class="box-content">
+                            <h3 class="box-title text-highlight">{{ $item['title'] }}</h3>
+                            <time datetime="{{ $item['time_markup'] }}">{{ $item['time_readable'] }} {{ $translations['ago'] }}</time>
+                            <p>{{ $item['excerpt'] }}</p>
+                            <p><span class="link-item">{{ $translations['readmore'] }}</span></p>
+                        </div>
+                    </a>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 @else
     <div class="notice info">
          <i class="pricon pricon-info-o"></i> {{ $translations['noposts'] }}
